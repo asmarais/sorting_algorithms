91dc51bb69773708495b2a0bed54eb074df7b07a
asmarais <rais.asma99@gmail.com>
2023-07-21 02:24:20 -0700

    sorting algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,9 +23,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[mid])
 				mid = j;
 		}
-		temp = array[i];
-		array[i] = array[mid];
-		array[mid] = temp;
-		print_array(array, size);
+		if (i != mid)
+		{
+			temp = array[i];
+			array[i] = array[mid];
+			array[mid] = temp;
+			print_array(array, size);
+		}
 	}
 }
