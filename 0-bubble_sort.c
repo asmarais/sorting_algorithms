38d05501b492a19a28c9042ce0148a2cda1dc427
asmarais <rais.asma99@gmail.com>
2023-07-21 02:17:30 -0700

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,8 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	int swap;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		swap = 0;
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,8 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, mid;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		mid = i;
